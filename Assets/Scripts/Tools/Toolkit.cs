90ce59b01386d05d2ccf8c176f342534bce8be06
Thex <thex-pieddroit@hotmail.com>
2018-09-15 01:51:52 +0200

    Reserialized all files after unity version upgrade

--- Assets/Scripts/Tools/Toolkit.cs
+++ Assets/Scripts/Tools/Toolkit.cs
@@ -1,8 +1,33 @@
 ﻿
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections.Generic;
+
+
 static public class Toolkit
 {
 	static public float Sqrd(this float fMe)
 	{
 		return fMe * fMe;
 	}
+
+
+#if UNITY_EDITOR
+	[MenuItem("Tools/Reserialize all files")]
+	static private void ReserializeAllFiles()
+	{
+		string[] pAllDataObjectsGUIDs = AssetDatabase.FindAssets("", new string[] { "Assets" });
+
+		List<string> pDataObjectsPaths = new List<string>(pAllDataObjectsGUIDs.Length);
+		for (int i = 0; i < pAllDataObjectsGUIDs.Length; ++i)
+		{
+			string sPath = AssetDatabase.GUIDToAssetPath(pAllDataObjectsGUIDs[i]);
+			if (!sPath.EndsWith(".unity"))
+				pDataObjectsPaths.Add(sPath);
+		}
+
+		AssetDatabase.ForceReserializeAssets(pDataObjectsPaths, ForceReserializeAssetsOptions.ReserializeAssetsAndMetadata);
+	}
+#endif
 }
