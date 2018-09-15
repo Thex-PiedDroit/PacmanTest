8852c993bfa5c34ecd4d6277e3140d126b14369d
Thex <thex-pieddroit@hotmail.com>
2018-09-15 14:38:54 +0200

    Added score updated display

---
+++ Assets/Scripts/Game/ScoresManager.cs
@@ -0,0 +1,38 @@
+﻿
+using TMPro;
+using UnityEngine;
+
+
+public class ScoresManager : MonoBehaviour
+{
+#region Variables (public)
+
+	public PlayerCharacter m_pPlayerCharacter = null;
+
+	public TextMeshProUGUI m_pScoreValueText = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void Awake()
+	{
+		m_pPlayerCharacter.m_pPickupsModule.OnPointsGained += UpdateScore;
+	}
+
+	private void OnDestroy()
+	{
+		if (m_pPlayerCharacter != null && m_pPlayerCharacter.m_pPickupsModule != null)
+			m_pPlayerCharacter.m_pPickupsModule.OnPointsGained -= UpdateScore;
+	}
+
+	private void UpdateScore(int iCurrentScore)
+	{
+		m_pScoreValueText.text = iCurrentScore.ToString();
+	}
+}
