c57f9782b71a33763f6100ced0b3fed408b9e6be
Thex <thex-pieddroit@hotmail.com>
2018-09-16 18:56:35 +0200

    Created hook prefab + small scene visual tweaks

---
+++ Assets/Scripts/Game/Pickups/GrapplingHook.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+
+public class GrapplingHook : MonoBehaviour
+{
+#region Variables (public)
+
+	public GameObject m_pRopeObject = null;
+
+	public LineRenderer m_pLineRenderer = null;
+	public Transform m_pRopeBegin = null;
+	public Transform m_pRopeEnd = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void LateUpdate()
+	{
+		if (m_pRopeObject.activeSelf)
+			UpdateRopeEnds();
+	}
+
+	private void UpdateRopeEnds()
+	{
+		m_pLineRenderer.SetPosition(0, m_pRopeBegin.position);
+		m_pLineRenderer.SetPosition(1, m_pRopeEnd.position);
+	}
+}
