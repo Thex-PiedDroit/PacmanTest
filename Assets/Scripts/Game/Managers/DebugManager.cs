09ed4fbcd18016250bde498ba571f6ef92ab4cc0
Thex <thex-pieddroit@hotmail.com>
2018-09-16 22:02:29 +0200

    Added DebugUI to easily give player a grappling hook for testing

---
+++ Assets/Scripts/Game/Managers/DebugManager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+
+public class DebugManager : MonoBehaviour
+{
+#region Variables (public)
+
+	public GrapplingHookEffect m_pGrapplingHookEffect = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	public void GiveGrapplingHookToPlayer()
+	{
+		if (GameManager.Instance.IsGamePlaying() && !PlayerCharacter.Instance.m_pPickupsModule.HasActiveEffect(m_pGrapplingHookEffect))
+			m_pGrapplingHookEffect.GiveEffectToPlayer(PlayerCharacter.Instance.m_pPickupsModule);
+	}
+}
